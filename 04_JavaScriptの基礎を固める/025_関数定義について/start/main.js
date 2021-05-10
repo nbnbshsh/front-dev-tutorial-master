b22502a7fd13a672785b4b72b904850fe6ae7a69
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-05-10 19:14:54 +0900

    JS 関数定義

---
+++ 04_JavaScriptの基礎を固める/025_関数定義について/start/main.js
@@ -0,0 +1,10 @@
+const hello=function(name, age){
+  // let name= 'Code mafia';
+  console.log('hello '+ name + age);
+  return name+age ;
+}
+
+hello('Code mafia',10);
+ const rValue=hello('Code mafia2',20);
+ console.log(rValue);
+

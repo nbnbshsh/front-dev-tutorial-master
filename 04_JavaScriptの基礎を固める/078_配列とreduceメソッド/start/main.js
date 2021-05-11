ea977fe05067ff3d3278f19ae6d08da613702571
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-05-11 22:47:21 +0900

    reduceメソッド

--- 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
+++ 04_JavaScriptの基礎を固める/078_配列とreduceメソッド/start/main.js
@@ -1,1 +1,25 @@
 const arry = [1,2,3,4,5];
+
+arry.reduce(function(accu,curr){
+  console.log(accu,curr);
+  return accu+curr;
+})
+
+arry.reduce(function(accu,curr){
+  console.log(accu,curr);
+  return accu+curr;
+},10)
+
+const str="animation";
+const strArry=str.split('');
+
+const result = strArry.reduce((accur,curr)=>{
+ return accur+"<"+curr+">";
+},"")
+
+const result1 = strArry.reduce((accur,curr)=>{
+  return `${accur}<${curr}>`;
+ },"")
+
+ console.log(result1);
+ console.log(result);

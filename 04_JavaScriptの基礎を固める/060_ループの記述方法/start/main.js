e325ae75e9237e73b021046ff5d517a0c9460b3c
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-05-11 13:57:15 +0900

    JS 簡単なfor文

---
+++ 04_JavaScriptの基礎を固める/060_ループの記述方法/start/main.js
@@ -0,0 +1,13 @@
+const arry=[1,2,3,4,5];
+
+for(let i=0; i<5 ; i=i+1){
+  console.log(i);
+}
+
+for(let i=0; i<4 ; i=i+1){
+  console.log(arry[i]);
+}
+
+for(let i=0;i<arry.length; i++){
+  console.log(arry[i]);
+}

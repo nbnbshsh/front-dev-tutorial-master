261cd7e8a04700c13dc33791cc58aa854f6e740f
nbnbshsh <hikonyanwan@hotmail.co.jp>
2021-05-11 16:10:08 +0900

    JS 比較演算子

---
+++ 04_JavaScriptの基礎を固める/065_比較演算子について/start/main.js
@@ -0,0 +1,67 @@
+const number2=0;
+
+if(!number2){
+  console.log("this is true");
+}else{
+  console.log("this is false");
+}
+
+
+const number1=null;
+const bool1=Boolean(number1);
+console.log(bool1);
+
+if(number1){
+  console.log("this is true");
+}else{
+  console.log("this is false");
+}
+
+
+const number=10;
+const bool=Boolean(number);
+console.log(bool);
+
+if(number){
+  console.log("this is true");
+}else{
+  console.log("this is false");
+}
+
+
+
+if(0=="0"&& 0==="0"){
+  console.log("this is true");
+}else{
+  console.log("this is false");
+}
+
+
+const num=Number(true);
+console.log(num);
+const num1=Number(false);
+console.log(num1);
+
+if(1=="1"){
+console.log("this is true");
+}else{
+  console.log("this is false");
+}
+
+if(1==="1"){
+  console.log("this is true");
+  }else{
+    console.log("this is false");
+  }
+
+  if(1===1){
+    console.log("this is true");
+    }else{
+      console.log("this is false");
+    }
+
+  if(1==true){
+    console.log("this is true");
+    }else{
+      console.log("this is false");
+    }
